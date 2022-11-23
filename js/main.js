2ec8dfa9323f6a5cccb20503b91c16076d536941
Aaron <payne.a@outlook.com>
2022-11-23 11:41:04 +0800

    updated menu and links

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-'use strict';
-
+var logo = document.getElementById('logo');
 /* logo animation */
-const logo = document.getElementById('logo');
 function logoAnimate(e) {
     if (e.currentTarget.classList.contains('animate-logo')) {
       e.currentTarget.classList.remove('animate-logo');
@@ -10,7 +8,46 @@ function logoAnimate(e) {
       e.currentTarget.classList.add('animate-logo');
       e.currentTarget.classList.remove('animate-logo2');
     }
-    document.getElementById("logo").onClick = logoAnimate();
 }
 
-logo.addEventListener('click', logoAnimate);
+var y = document.getElementById("h_nav");
+var element = document.getElementById("head");
+var x = document.getElementById("logo");
+function mobMenu() {
+  if (y.style.display === "block") {
+    y.style.display = "none";
+    x.style.display = "block";
+    element.style.height = "120px";
+    document.getElementById("menu-btn").className = ('fa-solid fa-bars');
+  } else {
+    y.style.display = "block";
+    x.style.display = "none";
+    element.style.height = "100vh";
+    document.getElementById("menu-btn").className = ('fa-solid fa-xmark');
+  }
+}
+
+/* Show the menu if window is re-sized to mobile then back to desktop size */
+function showMenu() {
+var mqdt = window.matchMedia( "(min-width: 768px)" );
+var mqmb = window.matchMedia( "(max-width: 768px)" );
+element.style.height = "120px";
+x.style.display = "block";
+document.getElementById("menu-btn").className = ('fa-solid fa-bars');
+if (mqdt.matches) {
+    y.style.display = "inline";  // window width is at least than 1050px
+  }
+if (mqmb.matches) {
+    y.style.display = "none";  // window width is less than 1050px
+}
+}
+
+function init() {
+  'use strict';
+  document.getElementById('menu-btn').onclick = mobMenu;
+  logo.addEventListener('click', logoAnimate);
+  window.onresize = showMenu;
+}
+
+window.onload = init;
+
